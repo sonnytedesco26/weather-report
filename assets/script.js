3a4d3a90853bfe84f06f77171428bf1ab661e9b7
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-02-07 18:05:25 -0500

    Wrote up variables in js, finished most html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
-bsjn
+const cityInput = document.getElementById("cityInput");
+const cityName = document.getElementById("cityNames");
+const search = document.getElementById("search");
+const clear = document.getElementById("clear");
+var weatherToday = document.getElementById("weatherToday");
+const currentPic = document.getElementById("currentPic");
+const temp = document.getElementById("temp");
+const wind = document.getElementById("wind");
+const humidity = document.getElementById("humidity");
+const UVIndex = document.getElementById("UVIndex");
+var fiveDay = document.getElementById("fiveDay");
+var forecast = document.getElementById("forecast")
+
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+
+const apiKey = "0ac4aa7b6f2b871c17671fb3f1fe4825";
+
