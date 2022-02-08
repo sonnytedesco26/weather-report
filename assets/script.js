93595f34d173e20afdafd2b4a0e9179bf90acf39
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-02-07 19:24:40 -0500

    started to add some api calling to newWeather function, some other js stuff

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 const cityInput = document.getElementById("cityInput");
-const cityName = document.getElementById("cityNames");
+const cityName = document.getElementById("cityName");
 const search = document.getElementById("search");
 const clear = document.getElementById("clear");
 var weatherToday = document.getElementById("weatherToday");
@@ -15,3 +15,28 @@ var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
 
 const apiKey = "0ac4aa7b6f2b871c17671fb3f1fe4825";
 
+function newWeather(cityName){
+
+    var URL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    
+    fetch(URL)
+    .then(function(r){
+        const year = moment().year();
+        const month = moment().month();
+        const day = moment().day();
+
+        cityInput.innerHTML = r.data.name + "(" + month + "/" + day + "/" + year +")";
+        var apiPic = r.data.weather.icon;
+        currentPic.setAttribute("src", "https://openweathermap.org/img/wn/" + apiPic + ".png");
+        currentPic.setAttribute("alt", r.data.weather.description);
+        temp.innerHTML = "Temp: " + Math.floor(((r.data.main.temp)-273.15)*1.8 + 32) + "&#176F";
+        wind.innerHTML = "Wind: " + r.data.wind.speed + "MPH";
+        humidity.innerHTML = "Humidity: " + r.data.main.humidity + "%";
+
+    })
+
+    clear.addEventListener("click", function(){
+        localStorage.clear();
+        searchHistory = [];
+    })
+}
