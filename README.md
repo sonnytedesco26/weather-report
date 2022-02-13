a68e79dd7b7b8a067bee5c6e2a56c7ce11bd4fa8
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-02-13 02:56:56 -0500

    realized last second that the UV had to be color changing, added comments and wrote up readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Weather Report
+LINK:
+https://sonnytedesco26.github.io/weather-report/
+
+For this project, the goal was to create a Weather Dashboard which would display the weather for the current day + 5 next days based on user input. This required the OpenWeather API to be called to load the data.
+
+## Inital Page
+
+![emptyWeather](/assets/images/emptyWeather.png)
+
+The user is presented with the search by city, and empty stats for the weather. The 5 day cards for the future forecast will not show up if nothing is entered. If a user attempts to press the 'Search' button without entering anything, an alert will yell at them to enter a name.
+
+## Loaded Page
+
+![WeatherCallExample](/assets/images/WeatherCallExample.png)
+
+When entering a city, the data for the current day is displayed in the big card to the right. This info, unlike the 5 day forecast, also includes the UV index, which changes based on how severe it is (Red for severe, grey for moderate, and green for good). Each search will be listed below search button, where the user will be able to click on each one to revisit their past forecast searches. There is also a 'Clear History' button, where the localStorage is emptied and the page goes back to blank. With the 5 day forecast, there was initially an issue where more than just 5 days would be returned from the API. Upon further examination, it became clear that each day was having numerous JSONs returned for it, all weather stats changing based on time of day. This issue was resolved by placing an if statement right after the for loop, making sure that no single day was repeated twice. The datefield had to be reformatted from the original API, as well as the temperature (given in Kelvin).
+
+
+
--- assets/script2.js
+++ assets/script2.js
@@ -23,7 +23,7 @@ var year = String(currentDate.getFullYear());
 var month = String(currentDate.getMonth() + 1);
 var day = String(currentDate.getDate());
 var currentDate = `${month}/${day}/${year}`;
-
+//Checks to see if local history is empty
 if (JSON.parse(localStorage.getItem("searchHistory")) == null) {
     console.log("searchHistory empty")
 } else {
@@ -31,11 +31,14 @@ if (JSON.parse(localStorage.getItem("searchHistory")) == null) {
 }
 
 searchButton.on("click", function(r) {
+    //prevents scary localstorage stuff
     r.preventDefault();
+    //yells at user for leaving input empty
     if (cityInput.val() === "") {
         window.alert("Please enter a city");
         return;
     }
+    //sets value for the city input, and calls the getweather function
     getWeather(cityInput.val());
 })
 
@@ -43,8 +46,8 @@ clearButton.on("click", function() {
     localStorage.clear();
     renderHistory("");
 })
-
-$(document).on("click", ".clickHistory", function() {
+//lets user click elements from history to recall the getweather function
+$(document).on("click", "#clickHistory", function() {
     getWeather($(this).text());
 })
 
@@ -62,45 +65,46 @@ function getWeather(object) {
                 cityWind: data.wind.speed,
                 cityHumidity: data.main.humidity,
                 cityUVIndex: data.coord,
+                //more than one weather returned, so only take the first img 
                 cityWeatherPic: data.weather[0].icon
             }
-
+            // UV Index needed different URL for API
             var UVURL = `https://api.openweathermap.org/data/2.5/uvi?lon=${cityDisplay.cityUVIndex.lon}&lat=${cityDisplay.cityUVIndex.lat}&appid=${apiKey}`
             $.ajax({
                     url: UVURL,
                     method: 'GET'
                 })
                 .then(function(uvStuff) {
+                    //checks to see if search history is completely empty
                     if (JSON.parse(localStorage.getItem("searchHistory")) == null) {
                         var loadedWeatherPic = `https:///openweathermap.org/img/w/${cityDisplay.cityWeatherPic}.png`;
                         var historyList = [];
-                        //if city name not present in list
-                        if (historyList.indexOf(cityDisplay.name) === -1) {
+                            //add city to list, set in local storage, and render the Weather/History
                             historyList.push(cityDisplay.name);
                             localStorage.setItem("searchHistory", JSON.stringify(historyList));
                             renderWeather(cityDisplay.name, cityDisplay.cityTemp, cityDisplay.cityWind, cityDisplay.cityHumidity, uvStuff.value, loadedWeatherPic);
                             renderHistory(cityDisplay.name);
-                        } else {
-                            renderWeather(cityDisplay.name, cityDisplay.cityTemp, cityDisplay.cityWind, cityDisplay.cityHumidity, uvStuff.value, loadedWeatherPic);
-                        }
+                        
 
                     } else {
                         var historyList = JSON.parse(localStorage.getItem("searchHistory"));
                         var loadedWeatherPic = `https:///openweathermap.org/img/w/${cityDisplay.cityWeatherPic}.png`;
+                        //checks to see if specific city is already in the search history
                         if (historyList.indexOf(cityDisplay.name) === -1) {
-
                             historyList.push(cityDisplay.name);
                             localStorage.setItem("searchHistory", JSON.stringify(historyList));
 
                             renderWeather(cityDisplay.name, cityDisplay.cityTemp, cityDisplay.cityWind, cityDisplay.cityHumidity, uvStuff.value, loadedWeatherPic);
                             renderHistory(cityDisplay.name);
                         } else {
+                            //if city already in history, no need to re-render the history
                             renderWeather(cityDisplay.name, cityDisplay.cityTemp, cityDisplay.cityWind, cityDisplay.cityHumidity, uvStuff.value, loadedWeatherPic);
                         }
                     }
                 })
 
                 function getForecast() {
+                    //needed to empty out the last search (otherwise, 5 days will stack based on search)
                     forecast.empty();
                     var fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${object}&APPID=${apiKey}`;
                     $.ajax({
@@ -109,23 +113,24 @@ function getWeather(object) {
                     })
                     .then(function(fivedayForecast){
                         for(i=0; i< fivedayForecast.list.length; i++){
+                            //API would return multiple entries for everyday (new stats every 3 hours), so if checks to see if the next entry in the loop has the same date
+                            //Also, dates returned included time fields, so need substring to capture only YYYY-MM-DD
                             if(fivedayForecast.list[i].dt_txt.substring(0,10) != fivedayForecast.list[i+1].dt_txt.substring(0,10)){
                             var fivedayDisplay={
-                                date: fivedayForecast.list[i].dt_txt,
-                                pic: fivedayForecast.list[i].weather[0].icon,
-                                temp: fivedayForecast.list[i].main.temp,
-                                wind: fivedayForecast.list[i].wind.speed,
-                                humidity: fivedayForecast.list[i].main.humidity
+                                //needs to be i+1 to get it to be next day
+                                date: fivedayForecast.list[i+1].dt_txt,
+                                pic: fivedayForecast.list[i+1].weather[0].icon,
+                                temp: fivedayForecast.list[i+1].main.temp,
+                                wind: fivedayForecast.list[i+1].wind.speed,
+                                humidity: fivedayForecast.list[i+1].main.humidity
                             }
                             var fivedayPic = `https:///openweathermap.org/img/w/${fivedayDisplay.pic}.png`;
                             createForecast(fivedayDisplay.date, fivedayPic, fivedayDisplay.temp, fivedayDisplay.wind, fivedayDisplay.humidity);
-                            console.log(fivedayForecast)
-                        }
+                       }
                         }
                     })
                 
                 }
-
                 getForecast();
         })
 }
@@ -133,22 +138,34 @@ function getWeather(object) {
 function renderWeather(name, cityTemp, cityWind, cityHumidity, cityUVIndex, cityWeatherPic) {
     cityName.text(name);
     cityDate.text(`${currentDate}`);
+    //temperature returned from API is kelvin
     temp.text(`Temp: ${Math.floor(((cityTemp-273.15)*1.8 + 32))} °F`);
     wind.text(`Wind: ${cityWind} MPH`);
     humidity.text(`Humidity: ${cityHumidity} %`);
-    UVIndex.text(`UV Index: ${cityUVIndex}`);
+    //Checks to see how severe the UVIndex is, then sets the class depending on the severity
+    if(cityUVIndex >=0 && cityUVIndex < 3){
+        $("#UVIndex").attr("class", "UVGood")
+        UVIndex.text(`UV Index: ${cityUVIndex}`);
+    } else if (cityUVIndex >=3 && cityUVIndex < 7){
+        $("#UVIndex").attr("class", "UVNormal")
+        UVIndex.text(`UV Index: ${cityUVIndex}`);
+    } else if (cityUVIndex >= 7){
+        $("#UVIndex").attr("class", "UVTrash")
+        UVIndex.text(`UV Index: ${cityUVIndex}`);
+    }
+    //UVIndex.text(`UV Index: ${cityUVIndex}`);
     weatherPic.attr("src", cityWeatherPic);
 }
 
 function createForecast(date, pic, temp, wind, humidity) {
     var forecastFiveDay = $("<div>").attr("id", "forecastFiveDay");
-    var forecastDate = $("<h3>").attr("id", "forecastDate");
-    var forecastPic = $("<img>").attr("id", "forecastPic");
-    var forecastTemp = $("<p>").attr("id", "forecastTemp");
-    var forecastWind = $("<p>").attr("id", "forecastWind");
-    var forecastHumid = $("<p>").attr("id", "forecastHumid");
+    var forecastDate = $("<h3>");
+    var forecastPic = $("<img>");
+    var forecastTemp = $("<p>");
+    var forecastWind = $("<p>");
+    var forecastHumid = $("<p>");
 
-
+    //appens div to have the following elements displayed
     forecast.append(forecastFiveDay);
     forecastDate.text(date.substring(0,10));
     forecastPic.attr("src", pic);
@@ -163,11 +180,10 @@ function renderHistory(name) {
     pastSearches.empty()
 
     var historyList = JSON.parse(localStorage.getItem("searchHistory"));
-    if(localStorage.length != 0){
     for (i = 0; i < historyList.length; i++) {
-        var newHistoryItem = $("<div>").attr("class", "clickHistory");
+        var newHistoryItem = $("<div>").attr("id", "clickHistory");
         newHistoryItem.text(historyList[i]);
+        //add new entries to top of the list
         pastSearches.prepend(newHistoryItem);
     }
-    }  
 }
